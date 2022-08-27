8d02904989e37027230c25896fdb6d8133fc8ee3
Luan M. Marques <luanmarques1789@gmail.com>
2022-08-27 16:56:55 -0300

    Environment variables (.env)

--- bootstrap.php
+++ bootstrap.php
@@ -2,5 +2,26 @@
 
 include_once __DIR__ . '/vendor/autoload.php';
 
-// define('URL', 'http://localhost');
-if (!defined('URL')) define('URL', 'http://localhost:5000');
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+if (!defined('URL'))
+  /** PROJECT URL */
+  define('URL', $_ENV['URL']);
+
+if (!defined('DB_USER'))
+  /** DATABASE'S USER */
+  define('DB_USER', $_ENV['DB_USER']);
+
+if (!defined('DB_PASSWORD'))
+  /** DATABASE'S USER PASSWORD */
+  define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
+
+if (!defined('DB_NAME'))
+  /** DATABASE'S NAME */
+  define('DB_NAME', $_ENV['DB_NAME']);
+
+if (!defined('DB_HOST'))
+  /** DATABASE'S HOST */
+  define('DB_HOST', $_ENV['DB_HOST']);
